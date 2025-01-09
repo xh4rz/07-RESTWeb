74408bb85a0873929a7243ec78c38986c80d7000
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2025-01-09 00:36:32 -0500

    192. Responder demás archivos

--- src/app.ts
+++ src/app.ts
@@ -2,17 +2,27 @@ import fs from 'fs';
 import http from 'http';
 
 const server = http.createServer((req, res) => {
+	console.log(req.url);
+
 	if (req.url === '/') {
 		const htmlFile = fs.readFileSync('./public/index.html', 'utf-8');
 
 		res.writeHead(200, { 'Content-Type': 'text/html' });
 
 		res.end(htmlFile);
-	} else {
-		res.writeHead(404, { 'Content-Type': 'text/html' });
 
-		res.end();
+		return;
 	}
+
+	if (req.url?.endsWith('.js')) {
+		res.writeHead(200, { 'Content-Type': 'application/javascript' });
+	} else if (req.url?.endsWith('.css')) {
+		res.writeHead(200, { 'Content-Type': 'text/css' });
+	}
+
+	const responseContent = fs.readFileSync(`./public${req.url}`, 'utf-8');
+
+	res.end(responseContent);
 });
 
 server.listen(8080, () => {
