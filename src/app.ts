6787aeb2a9ae1791cc9d97e53159270b828b1ad2
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2025-01-09 00:25:15 -0500

    191. Diferentes respuestas

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
+import fs from 'fs';
 import http from 'http';
 
 const server = http.createServer((req, res) => {
-	console.log(req.url);
+	if (req.url === '/') {
+		const htmlFile = fs.readFileSync('./public/index.html', 'utf-8');
 
-	res.write('Hola Mundo');
+		res.writeHead(200, { 'Content-Type': 'text/html' });
 
-	res.end();
+		res.end(htmlFile);
+	} else {
+		res.writeHead(404, { 'Content-Type': 'text/html' });
+
+		res.end();
+	}
 });
 
 server.listen(8080, () => {
