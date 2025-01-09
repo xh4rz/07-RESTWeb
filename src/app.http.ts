2d29bcfa18c4d6c582bd654caf006cd4cb2df610
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2025-01-09 03:28:26 -0500

    194. Express

---
+++ src/app.http.ts
@@ -0,0 +1,35 @@
+import fs from 'fs';
+import http from 'http';
+
+const server = http.createServer((req, res) => {
+	console.log(req.url);
+
+	if (req.url === '/') {
+		const htmlFile = fs.readFileSync('./public/index.html', 'utf-8');
+
+		res.writeHead(200, { 'Content-Type': 'text/html' });
+
+		res.end(htmlFile);
+
+		return;
+	}
+
+	if (req.url?.endsWith('.js')) {
+		res.writeHead(200, { 'Content-Type': 'application/javascript' });
+	} else if (req.url?.endsWith('.css')) {
+		res.writeHead(200, { 'Content-Type': 'text/css' });
+	}
+
+	try {
+		const responseContent = fs.readFileSync(`./public${req.url}`, 'utf-8');
+
+		res.end(responseContent);
+	} catch (error) {
+		res.writeHead(404, { 'Content-Type': 'text/html' });
+		res.end();
+	}
+});
+
+server.listen(8080, () => {
+	console.log('Server running on port 8080');
+});
---
+++ src/presentation/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+
+export class Server {
+	private app = express();
+
+	async start() {
+		// Middlewares
+
+		// Public Folder
+		this.app.use(express.static('public'));
+
+		this.app.listen(3000, () => {
+			console.log(`Server running on port ${3000}`);
+		});
+	}
+}
