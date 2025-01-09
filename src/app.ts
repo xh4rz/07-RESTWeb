0215c1f34dfbcbb51725aec54a4fc104d06e0d8e
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2025-01-09 00:13:59 -0500

    190. Webserver http/1

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,13 @@
-console.log('Hola Mundo');
+import http from 'http';
+
+const server = http.createServer((req, res) => {
+	console.log(req.url);
+
+	res.write('Hola Mundo');
+
+	res.end();
+});
+
+server.listen(8080, () => {
+	console.log('Server running on port 8080');
+});
